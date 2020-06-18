a65f354419c0aa59bb36b4f0ef7e33f8a458d290
Daniel Silva <daniel_santos2002@outlook.com>
2020-06-18 10:22:21 -0300

    Aula 2

--- osworks-api/src/main/java/com/algawork/osworks/api/controller/ClienteController.java
+++ osworks-api/src/main/java/com/algawork/osworks/api/controller/ClienteController.java
@@ -1,35 +1,76 @@
 package com.algawork.osworks.api.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algawork.osworks.domain.model.Cliente;
+import com.algawork.osworks.domain.repository.ClienteRepository;
+import com.algawork.osworks.domain.service.CadastroClienteService;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
-
-	@GetMapping("/clientes")
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private CadastroClienteService cadastroCliente;
+	
+	@GetMapping
 	public List<Cliente> listar(){
-		List<Cliente> clientes = new ArrayList<>();
+		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
 		
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setName("João");
-		cliente1.setEmail("joao@email.com");
-		cliente1.setTelefone("35 912345678");
+		if(cliente.isPresent()) return ResponseEntity.ok(cliente.get());
 		
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setName("Maria");
-		cliente2.setEmail("maeia@email.com");
-		cliente2.setTelefone("77 912365678");
-
-		clientes.add(cliente1);
-		clientes.add(cliente2);
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
 		
-		return clientes;
+		return cadastroCliente.salvar(cliente);
+	}
+	
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,
+			@Valid @RequestBody Cliente cliente) {
+		if (!clienteRepository.existsById(clienteId)) return ResponseEntity.notFound().build();
+		
+		cliente.setId(clienteId);
+		cliente = cadastroCliente.salvar(cliente);
+		
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> excluir(@PathVariable Long clienteId){
+		
+		if (!clienteRepository.existsById(clienteId)) return ResponseEntity.notFound().build();
+		
+		cadastroCliente.excluir(clienteId);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
